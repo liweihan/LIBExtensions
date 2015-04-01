3ffd7c4ff40a0b7df2e34f469a845cd1d5f44d21
Weihan Li <liweihan@me.com>
2015-03-31 17:22:47 -0700

    remove object ext

--- Pod/Classes/NSObject+LIBExtension.h
+++ Pod/Classes/NSObject+LIBExtension.h
@@ -8,8 +8,8 @@
 
 @interface NSObject (LIBExtension)
 
-- (void)li_addObserverForName:(NSString *)name object:(id)obj queue:(NSOperationQueue *)queue usingBlock:(void (^)(NSNotification *note))block;
-- (void)li_addObserverForName:(NSString *)name usingBlock:(void (^)(NSNotification *note))block;
+//- (void)li_addObserverForName:(NSString *)name object:(id)obj queue:(NSOperationQueue *)queue usingBlock:(void (^)(NSNotification *note))block;
+//- (void)li_addObserverForName:(NSString *)name usingBlock:(void (^)(NSNotification *note))block;
 
 
 @end
